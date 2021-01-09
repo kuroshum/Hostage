5790f55ed02d8d47c085e412e84d46ac2a9e5ae2
kuroshum <kurora.shumpei@g.wakayama-u.jp>
2021-01-09 15:48:42 +0900

    デコイ機能を追加

---
+++ Assets/Scripts/Decoy.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Decoy : Token
+{
+    // 管理オブジェクト
+    public static TokenMgr<Decoy> parent = null;
+
+    private GameMgr gm;
+    private void SetGmaeMgr(GameMgr gm) { this.gm = gm; }
+
+    [SerializeField]
+    private int ID;
+    public void SetParam(int id) { ID = id; }
+
+    private int maxHp = 3;
+    private int hp;
+    public void SetHp(int hp) { this.hp = hp; }
+    public int GetHp() { return hp; }
+
+    public bool isDeath;
+    public void SetIsDeath(bool death) { isDeath = death; }
+    public bool GetIsDeath() { return isDeath; }
+
+    public static Decoy Add(int id, GameMgr gm, Vector3 pos)
+    {
+        Decoy d = parent.Add(pos.x, pos.y, pos.z);
+
+        d.SetParam(id);
+
+        d.SetGmaeMgr(gm);
+
+        d.SetIsDeath(false);
+
+        return d;
+    }
+
+    public void InitilizeHp()
+    {
+        SetHp(maxHp);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hp <= 0)
+        {
+            SetIsDeath(true);
+            GameMgr.decoyList.RemoveAt(ID);
+            Vanish();
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "EnemyShot")
+        {
+            hp--;
+            SetHp(hp);
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/DecoyGauge.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecoyGauge : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject decoyObj;
+
+    // デコイゲージ全削除＆HP分作成
+    public void SetDecoyGauge(int decoyNum)
+    {
+        // デコイゲージを一旦全削除
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+        //　現在の弾ゲージ数分のデコイゲージを作成
+        for (int i = 0; i < decoyNum; i++)
+        {
+            Instantiate(decoyObj, transform);
+        }
+    }
+    //　ダメージ分だけ削除
+    public void DeleteDecoyGauge(int damage)
+    {
+        for (int i = 0; i < damage; i++)
+        {
+            //　最後のデコイゲージを削除
+            Destroy(transform.GetChild(i).gameObject);
+        }
+    }
+}
+
