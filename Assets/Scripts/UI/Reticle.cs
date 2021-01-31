fba8c2a11a0db8d50b5dd802ba0064319a4b6c66
kuroshum <kurora.shumpei@g.wakayama-u.jp>
2021-01-31 17:31:57 +0900

    マウスカーソルをレティクルに変更(fix #26)

---
+++ Assets/Scripts/UI/Reticle.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Reticle : MonoBehaviour
+{
+
+    [SerializeField]
+    private Texture2D reticle;
+
+    [SerializeField]
+    private Texture2D reticleForcuse;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void RayReticle()
+    {
+        // メインカメラからクリックした地点のベクトルでRayを飛ばす
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // 
+        RaycastHit hit = new RaycastHit();
+
+        //レイを投げて何かのオブジェクトに当たった場合
+        if (Physics.Raycast(ray, out hit))
+        {
+            // 衝突したオブジェクトがステージかウェイポイントの場合はデコイ配置
+            if (hit.collider.tag == "EnemyReticle")
+            {
+                OnPointerEnter(reticleForcuse);
+            }
+            else
+            {
+                OnPointerEnter(reticle);
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RayReticle();
+    }
+
+    public void OnPointerEnter(Texture2D reticle)
+    {
+        Cursor.SetCursor(reticle, Vector2.zero, CursorMode.Auto);
+    }
+
+}
