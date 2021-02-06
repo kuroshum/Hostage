d0be17aa2f55bb3f78853a26672e17712d9ca82c
kuroshum <kurora.shumpei@g.wakayama-u.jp>
2021-02-06 23:21:36 +0900

    プレイヤーと敵の砲撃の軌道を描画する(fix #32)

--- Assets/Scripts/UI/Reticle.cs
+++ Assets/Scripts/UI/Reticle.cs
@@ -12,44 +12,114 @@ public class Reticle : MonoBehaviour
     [SerializeField]
     private Texture2D reticleForcuse;
 
+    private LineRenderer orbit;
+    private Material orbitMaterial;
+
+    private Color normalColor = new Color(0f, 175f, 255f);
+    private Color forcuseColor = new Color(255f, 0f, 0f);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        orbit = this.GetComponent<LineRenderer>();
+
+        orbitMaterial = orbit.material;
+        orbitMaterial.SetColor("_Color", normalColor);
+
+
+        Vector3[] orbitPos = new Vector3[]
+        {
+            this.transform.position,
+            new Vector3(this.transform.position.x+5, this.transform.position.y,this.transform.position.z+5)
+        };
+
+        orbit.SetPositions(orbitPos);
+
+        Color[] colors = new Color[]
+        {
+            new Color(0,0,1),
+            new Color(0,0,1)
+        };
+
+        orbit.startWidth = 0.1f;
+        orbit.endWidth = 0.1f;
+
     }
 
-    private void RayReticle()
+    private void ChangeReticle(Vector3 mousePos, Ray ray)
     {
         // メインカメラからクリックした地点のベクトルでRayを飛ばす
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         // 
-        RaycastHit hit = new RaycastHit();
+        RaycastHit hit;
 
         //レイを投げて何かのオブジェクトに当たった場合
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit, Vector3.Distance(mousePos, this.transform.position), ~(1 << 14)))
         {
             // 衝突したオブジェクトがステージかウェイポイントの場合はデコイ配置
-            if (hit.collider.tag == "EnemyReticle")
+            if (hit.collider.tag == "Enemy")
             {
-                OnPointerEnter(reticleForcuse);
+                OnPointerEnter(reticleForcuse, forcuseColor);
             }
             else
             {
-                OnPointerEnter(reticle);
+                OnPointerEnter(reticle, normalColor);
             }
-
         }
+        else
+        {
+            OnPointerEnter(reticle, normalColor);
+        }
+    }
+
+    private void DrawOrbit(Vector3 mousePos, Ray ray)
+    {
+        RaycastHit hit;
+
+        Vector3 targetPos = mousePos;
+        if (Physics.Raycast(ray, out hit, Vector3.Distance(mousePos, this.transform.position), ~(1 << 14)))
+        {
+            if (hit.collider.tag == "Wall")
+            {
+                targetPos = hit.point;
+                //Debug.Log("wall");
+            }
+        }
+
+        Vector3[] orbitPos = new Vector3[]
+        {
+            this.transform.position,
+            targetPos
+        };
+
+        Debug.DrawRay(ray.origin, (mousePos - this.transform.position), Color.red, 0.1f);
+        orbit.SetPositions(orbitPos);
     }
 
     // Update is called once per frame
     void Update()
     {
-        RayReticle();
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 15.0f;
+        mousePos.x += 5f;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        mousePos = new Vector3(mousePos.x, this.transform.position.y, mousePos.z);
+        mousePos = mousePos + (mousePos - this.transform.position).normalized;
+
+        Ray ray = new Ray(this.transform.position, (mousePos - this.transform.position).normalized);
+
+        // カーソルが敵の上かそれ以外でレティクルを変更
+        ChangeReticle(mousePos, ray);
+
+        // 
+        DrawOrbit(mousePos, ray);
+
     }
 
-    public void OnPointerEnter(Texture2D reticle)
+    public void OnPointerEnter(Texture2D reticle, Color color)
     {
         Cursor.SetCursor(reticle, Vector2.zero, CursorMode.Auto);
+        orbitMaterial.SetColor("_Color", color);
     }
 
 }
